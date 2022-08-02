913bae8d2b9781139deb0a6227d4fc078d307c7a
Sagnella Franco <Fransagn@gmail.com>
2022-08-02 17:52:30 -0300

    creo que lo arregle, y agregue el vencimiento

--- src/main/java/com/example/demo/controller/pagoController.java
+++ src/main/java/com/example/demo/controller/pagoController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.net.URI;
+import java.time.OffsetDateTime;
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -72,7 +73,8 @@ public class pagoController {
 //		TODO Validar el token del consumidor
 //		VALIDAR SI EL PAGO YA EXISTE, SIMPLEMENTE DEVUELVO LA URL, SI NO EXISTE, LE HAGO EL ALTA
 		
-		resumen.getPago().setEstadoPago("noPagado");
+		resumen.getPago().setEstadoPago("pendiente");
+		resumen.getPago().setNotificado(false);
 		Date date = new Date();
 		resumen.getPago().setFechaCreacion(date.getTime());
 		resumen.getPago().setFechaEstado(date.getTime());
@@ -142,109 +144,22 @@ public class pagoController {
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 //	MERCADOPAGO
 	
-//	ACA RECIBO RESPUESTA DE MERCADO PAGO, PERO EL REALIDAD TODO LO REFERENTE A NOTIFICAR EL APGO Y DEMAS YA LO HICE,
-//	ACA ESTOY LLEGANDO SOLAMENTE CUANDO SE HACE EL AUTORETURN O SE TOCA EL BOTON VOLVER EN MERCADO PAGO, LO UNICO QUE HACE ES REDIRIGIRME
-//	A LA PAGINA FINAL DE MI APP
-	@CrossOrigin(origins = "*")
-	@GetMapping(value = "/respuesta_mercadoPago/{id}")
-	public ModelAndView redirect(@PathVariable(value = "id") Long pago_id, @RequestParam(value="collection_id") Long collection_id, @RequestParam(value="collection_status") String collection_status, 
-							@RequestParam(value="payment_id") Long payment_id, @RequestParam(value="status") String status,
-							@RequestParam(value="external_reference") String external_reference, @RequestParam(value="payment_type") String payment_type,
-							@RequestParam(value="merchant_order_id") Long merchant_order_id, @RequestParam(value="preference_id") String preference_id,
-							@RequestParam(value="site_id") String site_id, @RequestParam(value="processing_mode") String processing_mode,
-							@RequestParam(value="merchant_account_id") String merchant_account_id) {
-
-		Pago pago = pagoService.findById(pago_id).get();
-
-		//Aca ya se actualizo mi base y se mando respuesta al cliente, redirijo a mi app a la pantalla de resultado
-		if(status.equals("approved")){
-			return new ModelAndView("redirect:"+pago.getBackUrl());			
-		}
-		else {
-			return new ModelAndView("redirect:"+pago.getBackUrl());
-		}
-
-	}
 	
 	
-	
-	
-//	ESTO EMULA EL BACK EXTERNO AL QUE YO LE MANDO EL RETORNO
-	@CrossOrigin(origins = "*")
-	@PostMapping(value = "/respuesta")
-	public String respuestaLoca(@RequestBody RespuestaLoca param1) {
-		return "retorno del consumidor al recibir la info que le mande, le llego piola";
-	}
-	
-	
-//	NOTIFICATION DE MERCADOPAGO, ACA RECIBO LA RESPUESTA SOBRE EL ESTADO DE UN PAGO
-//	Aca deberia: ir a buscar a mercadoPago toda la data del pago, una vez la recupero, mando el post con la notificacion al notification url que me pasen de SIE
-	@CrossOrigin(origins = "*")
-	@PostMapping(value = "/notificacion_mercadoPago/{id}")
-	public String respuestaLoca(@PathVariable(value="id") Long pago_id, @RequestBody NotificacionMP param1) {
-		
-//		return new ModelAndView("redirect:"+"https://www.google.com.ar/");
-		
-		RestTemplate rest = new RestTemplate();
-		Pago pago = pagoService.findById(pago_id).get();
-		ResponseEntity<RespuestaLocaMP> responseMP;
-		ResponseEntity<String> response;
-//		
-//		Voy a buscar a mercado pago el pago que me llego en la notificacion
-		try {
-			URI uriMP = new URI("https://api.mercadopago.com/v1/payments/"+param1.getData().getId()+"?access_token=TEST-1016222742358593-062310-6eaddcc1b5893e037fa1281c4a6abe16-683211147");	
-				
-			responseMP = rest.getForEntity(uriMP, RespuestaLocaMP.class);
-			System.out.println("Es la respuesta con el payment que me da la api de mercado pago, recibo bien el estado");
-			System.out.println(responseMP.getBody().status);
-			System.out.println(responseMP.getBody().status_detail);
-			System.out.println(responseMP.getBody().id);
-
-//			//Cuando recibo la respuesta de mercado pago primero creo una transacción
-//			//Se hizo una transaccion, con el estado que haya resultado		
-			Date date = new Date();
-			Transaccion trans = new Transaccion();
-			
-			trans.setIdPago(pago_id);
-			trans.setEstado(responseMP.getBody().status);
-			trans.setFechaEstado(date.getTime());
-			trans.setIdTransaccion(""+responseMP.getBody().status);
-			trans.setIdMedioPago((long) 1);
-			transaccionService.save(trans);
-			
-//			Ademas me fijo, si el estado de la transaccion fue aprobado, lo pongo al pago como pagado tambien
-			if(responseMP.getBody().status.equals("approved"))
-			{
-				pago.setEstadoPago("pagado");				
-				pago.setFechaEstado(date.getTime());
-				pagoService.save(pago);
-			}
-
-		
-//			//Despues de actualizar el pago en mi base, mando la respuesta al cliente (con el back url del pago)		
-			URI uri = new URI(pago.getNotificationUrl());	
-			
-//			En la respuesta le envio el estado de la transaccion, y el id suyo de la transaccion, que yo relacione con el pago
-			RespuestaLoca res = new RespuestaLoca();
-			res.estado = trans.getEstado();
-			res.idTransaccionConsumidor = pago.getIdTransaccionConsumidor();
-			
-			response = rest.postForEntity(uri, res, String.class);
-			System.out.println(response.getBody());
-			
-			
-		}catch(Exception e){
-			System.out.println(e.getMessage());
-		}
-		
-		return "a";
-	}
-	
-	
-	
-//ESTE ES AL QUE LE MANDO DESDE EL FRONT, ES EL QUE ME HACE FUNCAR MERCADO PAGO
+	//ESTE ES AL QUE LE MANDO DESDE EL FRONT, ES EL QUE ME HACE FUNCAR MERCADO PAGO
 	@CrossOrigin(origins = "*")
 	@PostMapping(value = "/creates/{id}")
 	public ResponseEntity<?> create(@PathVariable(value = "id") Long pago_id, @RequestBody Item item) throws MPException, MPApiException{
@@ -279,11 +194,24 @@ public class pagoController {
 			       .failure("medio-pagos.herokuapp.com/api/pagos/respuesta_mercadoPago/"+pago_id)
 			       .build();
 		
-
+		OffsetDateTime dateTimeNow = OffsetDateTime.now();	
+		OffsetDateTime dateTimeTo = dateTimeNow.plusHours(5);
+//			System.out.println(dateTimeNow);
+//			System.out.println(dateTimeTo);
 		
 		try {
 			//Con el binaryMode, solo recibo aprobado o rechazado, no me puede quedar pendiente			
-			PreferenceRequest request = PreferenceRequest.builder().items(items).backUrls(backUrls).autoReturn("approved").binaryMode(true).notificationUrl("https://medio-pagos.herokuapp.com/api/pagos/notificacion_mercadoPago/"+pago_id).build();
+			PreferenceRequest request = PreferenceRequest.builder()
+					.items(items)
+					.backUrls(backUrls)
+					.autoReturn("approved")
+					.binaryMode(true)
+					.notificationUrl("https://medio-pagos.herokuapp.com/api/pagos/notificacion_mercadoPago/"+pago_id)
+					.expires(true)
+					.expirationDateFrom(dateTimeNow)
+					.expirationDateTo(dateTimeTo)
+					.externalReference(""+pago_id)
+					.build();
 			preference =client.create(request);
 			return new ResponseEntity<>(preference,HttpStatus.OK);
 			
@@ -293,8 +221,171 @@ public class pagoController {
 		}catch (MPException e) {
 			System.out.println("TIRA MPEXCEPTION");
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.OK);	
-		}
+		}	
 	}
 	
+//	NOTIFICATION DE MERCADOPAGO, ACA RECIBO LA RESPUESTA SOBRE EL ESTADO DE UN PAGO
+//	Aca deberia: ir a buscar a mercadoPago toda la data del pago, una vez la recupero, mando el post con la notificacion al notification url que me pasen de SIE
+	@CrossOrigin(origins = "*")
+	@PostMapping(value = "/notificacion_mercadoPago/{id}")
+	public String respuestaLoca(@PathVariable(value="id") Long pago_id, @RequestBody NotificacionMP param1) {
+		
+//		return new ModelAndView("redirect:"+"https://www.google.com.ar/");
+		
+		RestTemplate rest = new RestTemplate();
+		Pago pago = pagoService.findById(pago_id).get();
+		ResponseEntity<RespuestaLocaMP> responseMP;
+		ResponseEntity<String> response;
+//		
+//		Voy a buscar a mercado pago el pago que me llego en la notificacion
+		try {
+			URI uriMP = new URI("https://api.mercadopago.com/v1/payments/"+param1.getData().getId()+"?access_token=TEST-1016222742358593-062310-6eaddcc1b5893e037fa1281c4a6abe16-683211147");	
+				
+			responseMP = rest.getForEntity(uriMP, RespuestaLocaMP.class);
+//			System.out.println("Es la respuesta con el payment que me da la api de mercado pago, recibo bien el estado");
+//			System.out.println(responseMP.getBody().status);
+//			System.out.println(responseMP.getBody().status_detail);
+//			System.out.println(responseMP.getBody().id);
+
+			//Cuando recibo la respuesta de mercado pago primero creo una transacción
+			//Se hizo una transaccion, con el estado que haya resultado		
+			Date date = new Date();
+			Transaccion trans = new Transaccion();
+			
+			trans.setIdPago(pago_id);
+			trans.setEstado(responseMP.getBody().status);
+			trans.setFechaEstado(date.getTime());
+			trans.setIdTransaccion(""+responseMP.getBody().status);
+			trans.setIdMedioPago((long) 1);
+			transaccionService.save(trans);
+			
+			//Actualizo el estado del pago tambien (con el mismo estado que me trajo la transaccion, asi despues, cuando se ejecute el cron, lo va a poder notificar)
+			pago.setEstadoPago(trans.getEstado());				
+			pago.setFechaEstado(date.getTime());
+			pagoService.save(pago);
+
+			
+//			Solo voy ba notificar si el pago esta aprobado (porque si fue rechazado MP me va a dejar intentar de nuevo,
+//			o puede salir de MP, pero intentar de nuevo desde mi portal, entonces no puedo notificar muchos pagos rechazados,
+//			tengo que estar seguro de que ese rechazo es definitivo, por eso la notificacion del rechazo se va a hacer tocando el boton cancelar en el portal,
+//			o con el cron)
+			if(pago.getEstadoPago().equals("approved"))
+			{		
+				//Despues de actualizar el pago en mi base, mando la respuesta al cliente (con el notification url del pago)		
+				URI uri = new URI(pago.getNotificationUrl());	
+				
+				//En la respuesta le envio el estado de la transaccion, y el id suyo de la transaccion, que yo relacione con el pago
+				RespuestaLoca res = new RespuestaLoca();
+				res.estado = trans.getEstado();
+				res.idTransaccionConsumidor = pago.getIdTransaccionConsumidor();
+				
+				response = rest.postForEntity(uri, res, String.class);
+				System.out.println(response.getBody());
+				
+			}
+			
+			
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+		return "a";
+	}
+	
+//	CANCELAR PAGO: ACA ME LLEGA DESDE MI APP, SI EN LA PANTALLA PRINCIPAL TOCAN CANCELAR
+//	EN ESTE PUNTO TENGO QUE CANCELAR EL PAGO, Y NOTIFICAR AL NOTIFICATION URL DEL CONSUMIDOR Y REDIRIGIR AL BACK URL DEL CONSUMIDOR
+	@CrossOrigin(origins = "*")
+	@GetMapping(value = "/cancelar_pago/{id}")
+	public ModelAndView cancelar(@PathVariable(value = "id") Long pago_id) {
+
+//		Actualizo pago con rechazado
+		Date date = new Date();
+		Pago pago = pagoService.findById(pago_id).get();
+		pago.setEstadoPago("rejected");				
+		pago.setFechaEstado(date.getTime());
+		pagoService.save(pago);
+		
+//		Notifico al cliente pago rechazado
+		try {
+			RestTemplate rest = new RestTemplate();
+			ResponseEntity<String> response;
+			//Despues de actualizar el pago en mi base, mando la respuesta al cliente (con el notification url del pago)		
+			URI uri = new URI(pago.getNotificationUrl());	
+			
+			//En la respuesta le envio el estado de la transaccion, y el id suyo de la transaccion, que yo relacione con el pago
+			RespuestaLoca res = new RespuestaLoca();
+			res.estado = pago.getEstadoPago();
+			res.idTransaccionConsumidor = pago.getIdTransaccionConsumidor();
+			
+			response = rest.postForEntity(uri, res, String.class);
+			System.out.println(response.getBody());
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		//Aca ya se actualizo mi base y se mando respuesta al cliente, redirijo al back url del pago
+		return new ModelAndView("redirect:"+pago.getBackUrl());
+	}
+	
+	
+	@CrossOrigin(origins = "*")
+	@GetMapping(value = "/confirmar_pago/{id}")
+	public ModelAndView confirmar(@PathVariable(value = "id") Long pago_id) {
+
+		Pago pago = pagoService.findById(pago_id).get();
+		return new ModelAndView("redirect:"+pago.getBackUrl());
+	}
+	
+	
+	
+//	ACA RECIBO RESPUESTA DE MERCADO PAGO, PERO EL REALIDAD TODO LO REFERENTE A NOTIFICAR EL APGO Y DEMAS YA LO HICE,
+//	ACA ESTOY LLEGANDO SOLAMENTE CUANDO SE HACE EL AUTORETURN O SE TOCA EL BOTON VOLVER EN MERCADO PAGO, LO UNICO QUE HACE ES REDIRIGIRME
+//	A LA PAGINA FINAL DE MI APP
+	@CrossOrigin(origins = "*")
+	@GetMapping(value = "/respuesta_mercadoPago/{id}")
+	public ModelAndView redirect(@PathVariable(value = "id") Long pago_id, @RequestParam(value="collection_id") Long collection_id, @RequestParam(value="collection_status") String collection_status, 
+							@RequestParam(value="payment_id") Long payment_id, @RequestParam(value="status") String status,
+							@RequestParam(value="external_reference") String external_reference, @RequestParam(value="payment_type") String payment_type,
+							@RequestParam(value="merchant_order_id") Long merchant_order_id, @RequestParam(value="preference_id") String preference_id,
+							@RequestParam(value="site_id") String site_id, @RequestParam(value="processing_mode") String processing_mode,
+							@RequestParam(value="merchant_account_id") String merchant_account_id) {
+
+		//Aca ya se actualizo mi base y se mando respuesta al cliente, redirijo a mi app a la pantalla de resultado
+		if(status.equals("approved")){
+			return new ModelAndView("redirect:"+"http://localhost:4200/confirmacion/aprobado/"+pago_id);			
+		}
+		else {
+			return new ModelAndView("redirect:"+"http://localhost:42200/principal/"+pago_id);
+		}
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	ESTO EMULA EL BACK EXTERNO AL QUE YO LE MANDO EL RETORNO
+	@CrossOrigin(origins = "*")
+	@PostMapping(value = "/respuesta")
+	public String respuestaLoca(@RequestBody RespuestaLoca param1) {
+		return "retorno del consumidor al recibir la info que le mande, le llego piola";
+	}
 	
 }
