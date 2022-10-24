41e8fcc2687ee142a7144385ce4be5d958673be9
Sagnella Franco <Fransagn@gmail.com>
2022-10-24 15:53:50 -0300

    system property para front url

--- src/main/java/com/example/demo/MedioPagoClienteApplication.java
+++ src/main/java/com/example/demo/MedioPagoClienteApplication.java
@@ -16,7 +16,7 @@ public class MedioPagoClienteApplication {
 	public static void main(String[] args) {
 
 //		CONFIGURO ENVIRONMENT PROPERTIES
-		System.setProperty("frontUrl", "http://localhost:4200/principal/");
+		System.setProperty("frontUrl", "http://node2-app-nodejs-pagos.apps.lab.okd.local/principal/");
 
 		SpringApplication.run(MedioPagoClienteApplication.class, args);
 		MercadoPagoConfig.setAccessToken("TEST-1016222742358593-062310-6eaddcc1b5893e037fa1281c4a6abe16-683211147");//DE JULI
